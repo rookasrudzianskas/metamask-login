245a923d9b9d2a4ee93a3954928f06a7ecae30f0
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:33:03 +0300

    Rokas

--- components/Messages.js
+++ components/Messages.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {ByMoralis, useMoralis, useMoralisQuery} from 'react-moralis';
+import SendMessage from "./SendMessage";
 const Messages = () => {
     const {user} = useMoralis();
 
@@ -12,10 +13,11 @@ const Messages = () => {
             {/*    Message */}
 
             </div>
-            <div>
+            <div className="flex justify-center">
                 {/*<SendMessage />*/}
+                <SendMessage />
             </div>
-            <div>
+            <div className="text-center text-gray-400 mt-5">
                 <p>You're up to date {user.getUsername()}! 🥳</p>
             </div>
         </div>
