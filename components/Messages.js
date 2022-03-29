47b61a82ad0d826b52b8d1052e199f360a90f60e
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:25:54 +0300

    Rokas

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {ByMoralis, useMoralis, useMoralisQuery} from 'react-moralis';
 const Messages = () => {
+    const {user} = useMoralis();
+
     return (
         <div className="pb-56">
             <div className="my-5">
                 <ByMoralis variant="dark" style={{marginLeft: 'auto', marginRight: 'auto'}} />
+            </div>
+            <div className="">
+            {/*    Message */}
+
+            </div>
+            <div>
+                {/*<SendMessage />*/}
+            </div>
+            <div>
+                <p>You're up to date {user.getUsername()}! 🥳</p>
             </div>
         </div>
     );
