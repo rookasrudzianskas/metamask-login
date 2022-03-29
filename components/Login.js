9a5052c210b3dc938f8723d1c4bd32a12c06bfcb
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 10:53:17 +0300

    Rokas

--- components/Login.js
+++ components/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
         <div className="bg-black relative">
             <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
             {/*    logo plus login button */}
-                <Image src="https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj " width={200} height={200} />
+                <Image className="object-cover rounded-full" src="https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj " width={200} height={200} />
                 <button>Login to the METAVERSE</button>
             </div>
             <div className="w-full h-screen">
