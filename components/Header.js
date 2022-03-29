5793f55dedd219784e0bab04253897467d7649c7
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 14:43:17 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useMoralis} from "react-moralis";
 
 const Header = () => {
+    const {user} = useMoralis();
+
     return (
         <div>
             <h1>I am the header</h1>
