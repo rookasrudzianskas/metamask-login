66072d6afd988339890131a1eff69524fcb29754
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 09:45:02 +0300

    Rokas

---
+++ components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>I am the login screen</h1>
+        </div>
+    );
+};
+
+export default Login;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Login from '../components/Login';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
         <h1>Welcome to the Metaverse world 🚀</h1>
+        <Login />
 
     </div>
   )
