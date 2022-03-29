356a2da78e222518759307f5145a2208201db6e5
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 14:58:02 +0300

    Rokas

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
                 <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
                     <Image className="rounded-full object-cover" layout="fill" src="https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj" />
                 </div>
-                <div className="h-48 w-48 relative lg:mx-auto border-pink-500">
+                <div className="h-48 w-48 relative lg:mx-auto border-pink-500 border-8 rounded-full">
                     <Avatar logoutOnPress={true} />
                 </div>
             </div>
