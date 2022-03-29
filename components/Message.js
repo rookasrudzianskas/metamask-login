42937c396e97e044ffc84cf34abdbd26cd9cb627
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 16:08:22 +0300

    Rokas

--- components/Message.js
+++ components/Message.js
@@ -6,7 +6,7 @@ const Message = ({message}) => {
     const isUserMessage = message.get('ethAddress') === user.get('ethAddress');
 
     return (
-        <div className="">
+        <div className={`flex items-end space-x-2 relative ${isUserMessage && 'justify-end'}`}>
             <div className={`flex space-x-4 px-3 py-3 rounded-lg ${isUserMessage ? 'rounded-bg-none bg-pink-300' : 'rounded-bl-none bg-blue-400'}`}>
                 <p>{message.get('message')}</p>
 
