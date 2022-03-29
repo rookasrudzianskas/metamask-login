b2734bd5dcfb08e2fade57261f8fa55d6b9a63b8
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:02:02 +0300

    ROkas

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     const {user} = useMoralis();
 
     return (
-        <div>
+        <div className="text-pink-500">
             <div className="grid">
                 <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
                     <Image className="rounded-full object-cover" layout="fill" src="https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj" />
@@ -15,6 +15,8 @@ const Header = () => {
                 <div className="h-48 w-48 relative lg:mx-auto border-pink-500 border-8 rounded-full">
                     <Avatar logoutOnPress={true} />
                 </div>
+                <h1 className="text-3xl ">Welcome to cloud METAVERSE</h1>
+                <h2 className="text-5xl font-bold truncate">{user.getUsername()}</h2>
             </div>
         </div>
     );
