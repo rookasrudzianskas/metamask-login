f7848576f5b96c3b71539475ad7a7bab65c42ca4
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 11:04:45 +0300

    Rokas

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from "next/image";
 
+
 const Login = () => {
     return (
         <div className="bg-black relative">
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Login from '../components/Login';
+import { useMoralis } from "react-moralis";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
     const isAuthenticated = false;
