6d3646b696206c5bac06ba9df1fd455489957703
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:37:32 +0300

    Rokas

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -5,11 +5,20 @@ const SendMessage = () => {
     const {user, Moralis } = useMoralis();
     const [message, setMessage] = React.useState('');
     const [error, setError] = React.useState('');
+    const sendMessage = (e) => {
+        e.preventDefault();
+        if(!message) {
+            setError('Message is required');
+            return;
+        }
+
+
+    }
 
     return (
         <form className="flex fixed bottom-10 bg-black opacity-80 px-6 py-4  max-w-2xl w-11/12 shadow-xl rounded-full border-4 border-blue-400">
             <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" className="flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5" placeholder={`Enter a Message ${user.getUsername()}...`}/>
-            <button className="font-bold text-pink-500">Send</button>
+            <button onClick={sendMessage} type={'submit'} className="font-bold text-pink-500">Send</button>
         </form>
     );
 };
