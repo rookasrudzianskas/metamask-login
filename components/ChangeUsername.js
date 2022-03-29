60c71f1ca12ed730f1be1f40c7fef0fafdf0a121
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:07:46 +0300

    Rokas

--- components/ChangeUsername.js
+++ components/ChangeUsername.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useMoralis} from "react-moralis";
 
 const ChangeUsername = () => {
+    const {setUserData, isUserUpdating, userError, user} = useMoralis();
+
+    const setUserName = () => {
+
+    }
     return (
         <div className="text-sm absolute top-5 right-5">
-            <button className=" hover:text-pink-700">
+            <button disabled={isUserUpdating} onClick={setUserName} className=" hover:text-pink-700">
                 Change your username
             </button>
         </div>
