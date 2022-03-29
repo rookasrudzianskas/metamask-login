b7ac3cf7d558f5805f07b8c47db10f277c591104
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 16:05:09 +0300

    Rokas

--- components/Message.js
+++ components/Message.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {useMoralis} from 'react-moralis';
 
 const Message = ({message}) => {
+    const {user} = useMoralis();
+    const isUserMessage = message.get('ethAddress') === user.get('ethAddress');
+
     return (
         <div className="">
             <p>{message.get('message')}</p>
