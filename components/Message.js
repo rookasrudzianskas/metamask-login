18eddfe3e5e9655a5be1ddbb95b04c8d7c13ffb0
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 16:10:55 +0300

    Rokas

--- components/Message.js
+++ components/Message.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useMoralis} from 'react-moralis';
+import Avatar from "./Avatar";
 
 const Message = ({message}) => {
     const {user} = useMoralis();
@@ -7,6 +8,9 @@ const Message = ({message}) => {
 
     return (
         <div className={`flex items-end space-x-2 relative ${isUserMessage && 'justify-end'}`}>
+            <div className={`relative h-8 w-8 ${isUserMessage && 'order-last'}`} >
+                <Avatar username={message.get('username')} />
+            </div>
             <div className={`flex space-x-4 px-3 py-3 rounded-lg ${isUserMessage ? 'rounded-bg-none bg-pink-300' : 'rounded-bl-none bg-blue-400'}`}>
                 <p>{message.get('message')}</p>
 
