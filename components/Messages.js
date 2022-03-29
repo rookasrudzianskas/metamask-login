1640b49c120b6bd3472d4408fab84e4f8dfbea53
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:22:02 +0300

    Rokas

--- components/Messages.js
+++ components/Messages.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {ByMoralis, useMoralis, useMoralisQuery} from 'react-moralis';
 const Messages = () => {
     return (
-        <div className="">
-            <h1>I am the messages</h1>
+        <div className="pb-56">
+            div.pb-56
+
         </div>
     );
 };
