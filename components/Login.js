dca9339c4e044008e9247fb4dee7353e00246332
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 10:52:29 +0300

    Rokas

--- components/Login.js
+++ components/Login.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const Login = () => {
     return (
-        <div className="bg-black relative text-white">
-            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full">
+        <div className="bg-black relative">
+            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
             {/*    logo plus login button */}
                 <Image src="https://yt3.ggpht.com/-CDERLAq3BNY7murpWzg3z9Qde3c9ZrRx59LlLEb1UzKDKZ_ckpTAOlYVQ5TJo9XTgJl2kh9bw=s900-c-k-c0x00ffffff-no-rj " width={200} height={200} />
                 <button>Login to the METAVERSE</button>
