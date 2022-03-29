02790718505f8a2c7ac1de4fccb8ccf0c85885c5
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:43:35 +0300

    Rokas

--- components/Messages.js
+++ components/Messages.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {ByMoralis, useMoralis, useMoralisQuery} from 'react-moralis';
 import SendMessage from "./SendMessage";
 const Messages = () => {
     const {user} = useMoralis();
+    const endOfMessagesRef = useRef(null);
 
     return (
         <div className="pb-56">
@@ -17,7 +18,7 @@ const Messages = () => {
                 {/*<SendMessage />*/}
                 <SendMessage />
             </div>
-            <div className="text-center text-gray-400 mt-5">
+            <div ref={endOfMessagesRef} className="text-center text-gray-400 mt-5">
                 <p>You're up to date {user.getUsername()}! 🥳</p>
             </div>
         </div>
