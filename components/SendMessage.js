53a041962358a79c394913fa78455646a6ce41f5
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:34:35 +0300

    Rokas

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -3,6 +3,9 @@ import {useMoralis} from "react-moralis";
 
 const SendMessage = () => {
     const {user, Moralis } = useMoralis();
+    const [message, setMessage] = React.useState('');
+    const [error, setError] = React.useState('');
+
     return (
         <form className="flex fixed bottom-10 bg-black opacity-80 px-6 py-4  max-w-2xl w-11/12 shadow-xl rounded-full border-4 border-blue-400">
             <input type="text" className="flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5" placeholder={`Enter a Message ${user.getUsername()}...`}/>
