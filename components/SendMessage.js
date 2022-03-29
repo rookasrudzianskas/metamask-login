2569c1bcb64ce057efc37c30ccf374ded13cf810
Rokas <rookas.rudzenskas@gmail.com>
2022-03-29 15:35:44 +0300

    Rokas

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -8,7 +8,7 @@ const SendMessage = () => {
 
     return (
         <form className="flex fixed bottom-10 bg-black opacity-80 px-6 py-4  max-w-2xl w-11/12 shadow-xl rounded-full border-4 border-blue-400">
-            <input type="text" className="flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5" placeholder={`Enter a Message ${user.getUsername()}...`}/>
+            <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" className="flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5" placeholder={`Enter a Message ${user.getUsername()}...`}/>
             <button className="font-bold text-pink-500">Send</button>
         </form>
     );
